c55146d6d77244e12b2952f6f6f2c346f64044da
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-09-24 20:22:15 +0900

    인프라 정보 코드 추가

---
+++ GrowGame/Assets/Script/Infras/InfraInfo.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfraInfo : MonoBehaviour
+{
+    [Header("기본 정보")]
+    public string title = "인프라 이름";
+    public int level = 0;
+    public int infraCount = 0;
+    public string status = "";
+
+    public Image infraImage;
+
+}
