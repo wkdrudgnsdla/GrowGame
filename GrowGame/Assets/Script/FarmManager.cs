b16922d57caffbb9aacd4f70f480b7286ce3ff30
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-11-06 17:58:54 +0900

    밀 완료

--- GrowGame/Assets/Script/FarmManager.cs
+++ GrowGame/Assets/Script/FarmManager.cs
@@ -10,11 +10,56 @@ public class FarmManager : MonoBehaviour
     public int totalCrops => basicCrops + IManager.siloCapacity + 
         IManager.storageCapacity;
 
-    public int nowWheat;
-    public int nowCarrot;
-    public int nowCucumber;
-    public int nowPotato;
-    public int nowOnion;
-    public int nowCrops => nowWheat + nowCarrot + nowCucumber + nowPotato + nowOnion;
+    public float nowWheat;
+    public float nowCarrot;
+    public float nowCucumber;
+    public float nowPotato;
+    public float nowOnion;
+    public float nowCrops => nowWheat + nowCarrot + nowCucumber + nowPotato + nowOnion;
 
+    private float[] perSecondBase = { 25f / 60f, 50f / 60f, 75f / 60f };
+    private float[] perSecondWater = { 50f / 60f, 100f / 60f, 150f / 60f };
+
+    public void Update()
+    {
+        if (nowCrops < totalCrops)
+        {
+            GrowWheat();
+        }
+    }
+
+    public void GrowWheat()
+    {
+        if (!IManager.wheatFarmWater)
+        {
+            switch (IManager.wheatFarmLevel)
+            {
+                case 1:
+                    nowWheat += perSecondBase[0] * Time.deltaTime;
+                    break;
+                case 2:
+                    nowWheat += perSecondBase[1] * Time.deltaTime;
+                    break;
+                case 3:
+                    nowWheat += perSecondBase[2] * Time.deltaTime;
+                    break;
+            }
+        }
+        else
+        {
+            switch (IManager.wheatFarmLevel)
+            {
+                case 1:
+                    nowWheat += perSecondWater[0] * Time.deltaTime;
+                    break;
+                case 2:
+                    nowWheat += perSecondWater[1] * Time.deltaTime;
+                    break;  
+                case 3:
+                    nowWheat += perSecondWater[2] * Time.deltaTime;
+                    break;
+            }
+        }
+       
+    }
 }
