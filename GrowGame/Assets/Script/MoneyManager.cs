5157174459b32cf5458af2dd677d403665c6024d
wkdrudgnsdla <s24032@gsm.hs.kr>
2026-03-10 15:56:46 +0900

    판매 후 오르는 타이밍 어색한거 수정, 돈 증가 배수 추가

--- GrowGame/Assets/Script/MoneyManager.cs
+++ GrowGame/Assets/Script/MoneyManager.cs
@@ -1,26 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class MoneyManager : MonoBehaviour
 {
     [SerializeField] private FarmManager fManager;
 
     public double Money;
-    public float TotalSellPrice;
+    public float TotalSellPrice => ((int) fManager.nowWheat * 500 + (int) fManager.nowCarrot* 900 + (int) fManager.nowCucumber* 1400 + (int) fManager.nowPotato* 2200 + (int) fManager.nowOnion* 3500) * MoneyExtra;
 
-    private void FixedUpdate()
-    {
-        TotalSellPrice = (int)fManager.nowWheat * 500 + (int)fManager.nowCarrot * 900 + (int)fManager.nowCucumber * 1400 + (int)fManager.nowPotato * 2200 + (int)fManager.nowOnion * 3500;
-    }
+    public float MoneyExtra = 1;
+
 
     public void OnClickSell()
     {
         Money += TotalSellPrice;
-        fManager.nowWheat = 0;
-        fManager.nowCarrot = 0;
-        fManager.nowCucumber = 0;
-        fManager.nowPotato = 0;
-        fManager.nowOnion = 0;
+        fManager.nowWheat -= (int)fManager.nowWheat;
+        fManager.nowCarrot -= (int)fManager.nowCarrot;
+        fManager.nowCucumber -= (int)fManager.nowCucumber;
+        fManager.nowPotato -= (int)fManager.nowPotato;
+        fManager.nowOnion -= (int)fManager.nowOnion;
     }
 }
